5e32c6f72e0acf37de5939f815ea46a36f099f02
Taitanos <hayaashi87@gmail.com>
2023-12-04 20:35:40 +0300

    rename number1

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,7 +1,7 @@
 let number1 = Math.floor(Math.random() * 100)
 
-function even_or_odd(number) {
-    return number % 2 === 0 ? console.log(`[${number}] :Это четное число`) : console.log(`[${number}] :Это нечетное число`)
+function even_or_odd(number1) {
+    return number1 % 2 === 0 ? console.log(`[${number1}] :Это четное число`) : console.log(`[${number1}] :Это нечетное число`)
 }
 
 even_or_odd(number1)
