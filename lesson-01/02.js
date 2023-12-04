3466d200f08956726b56e7350d8e618b27387bb3
Taitanos <hayaashi87@gmail.com>
2023-12-04 20:19:26 +0300

    solution task 2

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,7 @@
-// code
+let number1 = Math.floor(Math.random() * 100)
+
+function even_or_odd(number) {
+    return number % 2 === 0 ? console.log(`[${number}] :Это четное число`) : console.log(`[${number}] :Это нечетное число`)
+}
+
+even_or_odd(number1)
